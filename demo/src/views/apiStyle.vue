5214bf102ce2d9420a4d740fdd7af9b6c4273efa
kimjuneseo <june3929213512@gmail.com>
2023-11-27 09:44:57 +0900

    vue 프로젝트 생성

---
+++ demo/src/views/apiStyle.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// 반응적인 상태의 속성
+const count = ref(0)
+
+// 속성 값을 변경하고 업데이트 할 수 있는 함수.
+function increment() {
+  count.value++
+}
+
+// 생명 주기 훅
+onMounted(() => {
+  console.log(`숫자 세기의 초기값은 ${ count.value } 입니다.`)
+})
+</script>
+
+<template>
+  <button @click="increment">숫자 세기: {{ count }}</button>
+</template>
