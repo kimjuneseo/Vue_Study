d2f900e6818cd0ba286be9688fa0ca34b3b3e82a
kimjuneseo <june3929213512@gmail.com>
2023-11-29 10:41:46 +0900

    옵션으로 쓰겠지?

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld @click="clickHello" msg="Welcome to Your Vue.js App"/> -->
   
   <List name="김준서"/>
+  <Template></Template>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Template from './components/Template.vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
@@ -14,7 +16,7 @@ export default {
     appName:String
   },
   components: {
-    HelloWorld
+    HelloWorld, Template
   },
   
   methods: {
---
+++ demo/src/components/Template.vue
@@ -0,0 +1,34 @@
+<template lang="">
+    <p>텍스트 보간법 사용: {{ rawHtml }}</p>
+    <div @click="updateHtml" v-html="rawHtml">
+        
+    </div>
+    <input v-bind="inputAttrs" :/>
+</template>
+<script>
+export default {
+    name: "Template",
+    data() {
+        return {
+            rawHtml: '<span style="color: red">이것은 빨간색이어야 합니다.</span>',
+            
+            inputAttrs: {
+                id: 'input',
+                class: 'input',
+                disabled: false
+            }
+        }
+    },
+    methods: {
+        updateHtml() {
+            this.rawHtml = "<span>김준서</span>"
+            this.inputAttrs.disabled = !this.inputAttrs.disabled;
+            console.log(this.rawHtml, this.isInputDisabled);
+            
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
