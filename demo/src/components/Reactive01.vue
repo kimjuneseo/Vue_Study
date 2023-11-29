e484ddb5b4320aad6020a937d9622499683328ed
kimjuneseo <june3929213512@gmail.com>
2023-11-29 15:15:04 +0900

    1

--- demo/src/components/Reactive01.vue
+++ demo/src/components/Reactive01.vue
@@ -1,11 +1,37 @@
 <template lang="">
-    <div>
-        응
-    </div>
+    <button @click="handleBtn">
+        Reactive {{count}}
+    </button>
+    <button @click="handleDebounceBtn">
+        de {{count}}
+    </button>
 </template>
 <script>
+import { debounce } from 'lodash-es'
+import { nextTick } from 'vue'
 export default {
-    name: 'Reactive'
+    name: 'Reactive',
+    data(){
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        handleBtn() {
+            this.count++;
+            // console.log(document.querySelector('button').innerText = "11");
+            nextTick(() => {
+                console.log(document.querySelector('button').innerText = "112");
+            })
+        },
+        handleDebounceBtn: debounce(function() {
+            this.count++;
+            // console.log(document.querySelector('button').innerText = "11");
+            nextTick(() => {
+                console.log(document.querySelector('button').innerText = "112");
+            })
+        }, 500)
+    }
 }
 </script>
 <style lang="">
