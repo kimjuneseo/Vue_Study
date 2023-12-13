d4f60bab301c9ce6bab94d70a8e76ad6cbe49a3d
kimjuneseo <june3929213512@gmail.com>
2023-12-13 16:58:54 +0900

    템플릿 참조..

---
+++ demo/src/components/TemplateRef.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+      <li v-for="(item, index) in list" ref="items" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </template>
+  
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        /* ... */
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$refs.items)
+  }
+}
+</script>
+
